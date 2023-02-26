d03422aba641372d13332f16ff4b226e2416521c
Darren Seah <darrenseahyongliang@protonmail.com>
2023-02-26 23:50:43 +0800

    Images and unique inputs implemented

--- lib/screens/calculator.dart
+++ lib/screens/calculator.dart
@@ -1,8 +1,90 @@
 import 'package:flutter/material.dart';
 import 'package:guinculator/widgets/buttons.dart';
 
-class Calculator extends StatelessWidget {
+class Calculator extends StatefulWidget {
   const Calculator({super.key});
+
+  @override
+  State<Calculator> createState() => _CalculatorState();
+}
+
+class _CalculatorState extends State<Calculator> {
+  /* Variables */
+  String answer = '';
+  String values = '';
+
+  /* Calculation */
+
+  /* Getting unique button inputs */
+  void updateValues(String btnData) {
+    setState(() {
+      switch (btnData) {
+        case "AC":
+          values = "";
+          answer = "";
+          break;
+        case "Brackets":
+          values = "Brackets";
+          break;
+        case "Percentage":
+          values = "Percentage";
+          break;
+        case "Divide":
+          values = "Divide";
+          break;
+        case "1":
+          values = "1";
+          break;
+        case "2":
+          values = "2";
+          break;
+        case "3":
+          values = "3";
+          break;
+        case "4":
+          values = "4";
+          break;
+        case "5":
+          values = "5";
+          break;
+        case "6":
+          values = "6";
+          break;
+        case "7":
+          values = "7";
+          break;
+        case "8":
+          values = "8";
+          break;
+        case "9":
+          values = "9";
+          break;
+        case "0":
+          values = "0";
+          break;
+        case "Times":
+          values = "Times";
+          break;
+        case "Minus":
+          values = "Minus";
+          break;
+        case "Plus":
+          values = "Plus";
+          break;
+        case "Dot":
+          values = ".";
+          break;
+        case "Back":
+          values = "Back";
+          break;
+        case "Equal":
+          values = "Equal";
+          break;
+        default:
+          break;
+      }
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -12,7 +94,8 @@ class Calculator extends StatelessWidget {
         child: Column(
           children: [
             Container(
-              alignment: Alignment.center,
+              alignment: Alignment.centerRight,
+              padding: const EdgeInsets.symmetric(vertical: 30, horizontal: 20),
               height: 300,
               width: MediaQuery.of(context).size.width,
               decoration: const BoxDecoration(
@@ -21,6 +104,25 @@ class Calculator extends StatelessWidget {
                   bottomLeft: Radius.circular(30),
                   bottomRight: Radius.circular(30),
                 ),
+              ),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: [
+                  Text(
+                    values,
+                    style: const TextStyle(fontSize: 70, color: Colors.white),
+                    overflow: TextOverflow.ellipsis,
+                    maxLines: 1,
+                  ),
+                  const SizedBox(height: 50),
+                  Text(
+                    answer,
+                    style: const TextStyle(fontSize: 70, color: Colors.white),
+                    overflow: TextOverflow.clip,
+                    maxLines: 1,
+                  )
+                ],
               ),
             ),
             Container(
@@ -38,45 +140,102 @@ class Calculator extends StatelessWidget {
                 children: [
                   Column(
                     mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: const [
+                    children: [
                       MyButtons(
-                          buttonVal: "AC", buttonColor: Color(0xFF244C5B)),
-                      MyButtons(buttonVal: "7", buttonColor: Color(0xFF244C5B)),
-                      MyButtons(buttonVal: "4", buttonColor: Color(0xFF244C5B)),
-                      MyButtons(buttonVal: "1", buttonColor: Color(0xFF244C5B)),
-                      MyButtons(buttonVal: "0", buttonColor: Color(0xFF244C5B)),
+                          updateValue: () => updateValues("AC"),
+                          buttonColor: const Color(0xFF244C5B),
+                          buttonIconImage: 'lib/icon/AC.png'),
+                      MyButtons(
+                          updateValue: () => updateValues("7"),
+                          buttonColor: const Color(0xFF244C5B),
+                          buttonIconImage: 'lib/icon/7.png'),
+                      MyButtons(
+                          updateValue: () => updateValues("4"),
+                          buttonColor: const Color(0xFF244C5B),
+                          buttonIconImage: 'lib/icon/4.png'),
+                      MyButtons(
+                          updateValue: () => updateValues("1"),
+                          buttonColor: const Color(0xFF244C5B),
+                          buttonIconImage: 'lib/icon/1.png'),
+                      MyButtons(
+                          updateValue: () => updateValues("0"),
+                          buttonColor: const Color(0xFF244C5B),
+                          buttonIconImage: 'lib/icon/Zero.png'),
                     ],
                   ),
                   Column(
                     mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: const [
+                    children: [
                       MyButtons(
-                          buttonVal: "( )", buttonColor: Color(0xFF344B3F)),
-                      MyButtons(buttonVal: "8", buttonColor: Color(0xFF1F2521)),
-                      MyButtons(buttonVal: "5", buttonColor: Color(0xFF1F2521)),
-                      MyButtons(buttonVal: "2", buttonColor: Color(0xFF1F2521)),
-                      MyButtons(buttonVal: ".", buttonColor: Color(0xFF1F2521)),
+                          updateValue: () => updateValues("Brackets"),
+                          buttonColor: const Color(0xFF344B3F),
+                          buttonIconImage: 'lib/icon/Brackets.png'),
+                      MyButtons(
+                          updateValue: () => updateValues("8"),
+                          buttonColor: const Color(0xFF1F2521),
+                          buttonIconImage: 'lib/icon/8.png'),
+                      MyButtons(
+                          updateValue: () => updateValues("5"),
+                          buttonColor: const Color(0xFF1F2521),
+                          buttonIconImage: 'lib/icon/5.png'),
+                      MyButtons(
+                          updateValue: () => updateValues("2"),
+                          buttonColor: const Color(0xFF1F2521),
+                          buttonIconImage: 'lib/icon/2.png'),
+                      MyButtons(
+                          updateValue: () => updateValues("Dot"),
+                          buttonColor: const Color(0xFF1F2521),
+                          buttonIconImage: 'lib/icon/Dot.png'),
                     ],
                   ),
                   Column(
                     mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: const [
-                      MyButtons(buttonVal: "%", buttonColor: Color(0xFF344B3F)),
-                      MyButtons(buttonVal: "9", buttonColor: Color(0xFF1F2521)),
-                      MyButtons(buttonVal: "6", buttonColor: Color(0xFF1F2521)),
-                      MyButtons(buttonVal: "3", buttonColor: Color(0xFF1F2521)),
+                    children: [
                       MyButtons(
-                          buttonVal: "Back", buttonColor: Color(0xFF1F2521)),
+                          updateValue: () => updateValues("Percentage"),
+                          buttonColor: const Color(0xFF344B3F),
+                          buttonIconImage: 'lib/icon/Percentage.png'),
+                      MyButtons(
+                          updateValue: () => updateValues("9"),
+                          buttonColor: const Color(0xFF1F2521),
+                          buttonIconImage: 'lib/icon/9.png'),
+                      MyButtons(
+                          updateValue: () => updateValues("6"),
+                          buttonColor: const Color(0xFF1F2521),
+                          buttonIconImage: 'lib/icon/6.png'),
+                      MyButtons(
+                          updateValue: () => updateValues("3"),
+                          buttonColor: const Color(0xFF1F2521),
+                          buttonIconImage: 'lib/icon/3.png'),
+                      MyButtons(
+                          updateValue: () => updateValues("Back"),
+                          buttonColor: const Color(0xFF1F2521),
+                          buttonIconImage: 'lib/icon/Back.png'),
                     ],
                   ),
                   Column(
                     mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: const [
-                      MyButtons(buttonVal: "/", buttonColor: Color(0xFF344B3F)),
-                      MyButtons(buttonVal: "X", buttonColor: Color(0xFF344B3F)),
-                      MyButtons(buttonVal: "-", buttonColor: Color(0xFF344B3F)),
-                      MyButtons(buttonVal: "+", buttonColor: Color(0xFF344B3F)),
-                      MyButtons(buttonVal: "=", buttonColor: Color(0xFF005231)),
+                    children: [
+                      MyButtons(
+                          updateValue: () => updateValues("Divide"),
+                          buttonColor: const Color(0xFF344B3F),
+                          buttonIconImage: 'lib/icon/Divide.png'),
+                      MyButtons(
+                          updateValue: () => updateValues("Times"),
+                          buttonColor: const Color(0xFF344B3F),
+                          buttonIconImage: 'lib/icon/Times.png'),
+                      MyButtons(
+                          updateValue: () => updateValues("Minus"),
+                          buttonColor: const Color(0xFF344B3F),
+                          buttonIconImage: 'lib/icon/Minus.png'),
+                      MyButtons(
+                          updateValue: () => updateValues("Plus"),
+                          buttonColor: const Color(0xFF344B3F),
+                          buttonIconImage: 'lib/icon/Plus.png'),
+                      MyButtons(
+                          updateValue: () => updateValues("Equal"),
+                          buttonColor: const Color(0xFF005231),
+                          buttonIconImage: 'lib/icon/Equal.png'),
                     ],
                   ),
                 ],
--- lib/widgets/buttons.dart
+++ lib/widgets/buttons.dart
@@ -1,23 +1,28 @@
-import 'package:fluentui_system_icons/fluentui_system_icons.dart';
 import 'package:flutter/material.dart';
 
 class MyButtons extends StatelessWidget {
-  final String buttonVal;
   final Color buttonColor;
-  const MyButtons(
-      {super.key, required this.buttonVal, required this.buttonColor});
+  final String buttonIconImage;
+  final Function()? updateValue;
+
+  const MyButtons({
+    super.key,
+    required this.buttonColor,
+    required this.buttonIconImage,
+    required this.updateValue,
+  });
 
   @override
   Widget build(BuildContext context) {
     return ElevatedButton(
-      onPressed: () {},
+      onPressed: updateValue,
       style: ElevatedButton.styleFrom(
         backgroundColor: buttonColor,
         fixedSize: Size(MediaQuery.of(context).size.width / 4 - 10,
             MediaQuery.of(context).size.width / 4 - 20),
         shape: const CircleBorder(),
       ),
-      child: const Icon(FluentIcons.equal_off_16_regular, size: 50),
+      child: Image.asset(buttonIconImage),
     );
   }
 }
